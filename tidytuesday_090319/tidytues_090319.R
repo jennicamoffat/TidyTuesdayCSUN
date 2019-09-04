3fb4b83c5c074ab7fd38a082864bca12ad05b5ba
Jennica Moffat <jennica.moffat.94@my.csun.edu>
2019-09-03 17:39:36 -0700

    Commit test second Tidy Tuesday

---
+++ tidytuesday_090319/tidytues_090319.R
@@ -0,0 +1,13 @@
+#TidyTuesday 9/3/2019
+
+library('tidyverse')
+#Clear the environment
+rm(list=ls())
+
+cpu <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-09-03/cpu.csv")
+gpu <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-09-03/gpu.csv")
+ram <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-09-03/ram.csv")
+
+View(cpu)
+View(gpu)
+View(ram)
