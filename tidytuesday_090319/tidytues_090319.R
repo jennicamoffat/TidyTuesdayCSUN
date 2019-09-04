d39e0fd4cbd18be0699b7ebf1f6475e4fbc0cb14
Jennica Moffat <jennica.moffat.94@my.csun.edu>
2019-09-03 18:13:10 -0700

    Made basic scatterplot

--- tidytuesday_090319/tidytues_090319.R
+++ tidytuesday_090319/tidytues_090319.R
@@ -13,9 +13,15 @@ View(cpu)
 View(gpu)
 View(ram)
 
+#Merge cpu and gpu
 df<-merge(x=cpu,y=gpu, by="date_of_introduction")
 View(df)
-
+#Merge new df with ram so all df's are merged
 alldata<-merge(x=df, y=ram, by="date_of_introduction")
 View(alldata)
+
+#Make basic line graph
+ggplot(alldata, aes(x=date_of_introduction, y=capacity_bits))+
+  geom_point()
+#Not interesting but at least I remember how to make a plot. 
 
