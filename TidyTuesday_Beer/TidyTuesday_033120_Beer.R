37a34ac508ce6d9d625ba0e3d3efddaa8e3e6f05
Jennica Moffat <jennica.moffat.94@my.csun.edu>
2020-03-31 18:38:24 -0700

    Cleaned up code.

--- TidyTuesday_Beer/TidyTuesday_033120_Beer.R
+++ TidyTuesday_Beer/TidyTuesday_033120_Beer.R
@@ -14,54 +14,43 @@ library(ggimage)
 rm(list=ls())
 #load data
 brewing_materials <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-31/brewing_materials.csv')
-beer_taxed <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-31/beer_taxed.csv')
-brewer_size <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-31/brewer_size.csv')
-beer_states <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-31/beer_states.csv')
 
 View(brewing_materials)
-View(beer_taxed)
-View(brewer_size)
-View(beer_states)
-
 brewing_materials$year<-as.factor(brewing_materials$year)
 
-#Basic connected scatter plot by year over the months
-brewing_materials  %>%
-  ggplot( aes(x=month, y=month_current, color=year)) +
-  geom_line()+
-  geom_point()
-#Everything is connected. Summarizing data.
+#Gonna just pull out "Total Used"
+brewing.data<-subset(brewing_materials, type=="Total Used")
+View(brewing.data)
+brewing.data$scaled.total<-(brewing.data$month_current)/1000000
 
-
-brewing<- brewing_materials %>%
-  group_by(year, month)%>%
-  summarize(mean=mean(month_current, na.rm=TRUE))
-View(brewing)
-brewing$scaled.mean<-(brewing$mean)/1000000
-brewing <- transform(brewing, MonthAbb = month.abb[month])
-
-#Connected scatter plot of barrels produced by year
-BeerPlot<-brewing %>%
-  ggplot(aes(x=month, y=scaled.mean, color=year))+
+#Plot of total
+Plot<-brewing.data %>%
+  ggplot(aes(x=month, y=scaled.total, color=year))+
   geom_line(size=1)+
   geom_point(size=2)+
   theme_bw()+
-  ggtitle("Average Barrels of Beer Produced by Year")+
+  ggtitle("Total Barrels of Beer Produced by Year")+
   scale_x_discrete(name="", limits = month.abb)+ #Transforms numbers to month abbreviation
-  scale_y_continuous(name="Avg Production (millions of barrels)")+
+  scale_y_continuous(name="Total Production (millions of barrels)")+
   theme(plot.title = element_text(size=14), axis.text.x=element_text(color="black", size=12), axis.text.y=element_text(size=12), axis.title.y = element_text(color="black", size=14))+
   labs(color="Year")
-BeerPlot
+Plot
 
 #Adding barrels to plot. 
 Barrel <- readPNG("Barrel.png")
 g <- rasterGrob(Barrel, interpolate=TRUE)
-BarrelPlot <- BeerPlot + annotation_custom(g, xmin=11, xmax=12, ymin=5, ymax=35)
+BarrelPlot <- Plot + annotation_custom(g, xmin=11, xmax=12, ymin=25, ymax=125)
 BarrelPlot
 
 Barrels <- readPNG("MultipleBarrels.png")
 g2 <- rasterGrob(Barrels, interpolate=TRUE)
-BarrelPlot2 <- BarrelPlot + annotation_custom(g2, xmin=0.5, xmax=4.5, ymin=50, ymax=120)
+BarrelPlot2 <- BarrelPlot + annotation_custom(g2, xmin=0.5, xmax=4.5, ymin=200, ymax=475)
 BarrelPlot2
 
-BarrelPlot2 +ggsave("BeerPlot.pdf", width=10, height=6.19, dpi=300, unit="in")
+#Adding text to label barrels
+BarrelPlot3<-BarrelPlot2+annotate("text", x = 11.5, y = 150, label = c("2016-2017"), color="brown", fontface="bold")
+BarrelPlot3
+BarrelPlot4<-BarrelPlot3+annotate("text", x = 4.2, y = 380, label = c("2008-2015"), color="brown", fontface="bold")
+BarrelPlot4
+
+BarrelPlot4 + ggsave("BeerPlot.pdf", width=10, height=6.19, dpi=300, unit="in")
