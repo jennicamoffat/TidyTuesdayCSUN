d0c978274a270be7e0ba987d59d7ddfd9db4a29b
Jennica Moffat <jennica.moffat.94@my.csun.edu>
2020-05-26 19:35:30 -0700

    Cleaned script

--- TidyTuesday_052620_Cocktails/TT_Cocktails.R
+++ TidyTuesday_052620_Cocktails/TT_Cocktails.R
@@ -3,7 +3,9 @@
 #Created by Jennica Moffat
 
 library(tidyverse)
-
+library(PNWColors)
+library(cowplot)
+library(ggpubr)
 
 #Clear environment
 rm(list=ls())
@@ -11,32 +13,72 @@ rm(list=ls())
 cocktails <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/cocktails.csv')
 boston_cocktails <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/boston_cocktails.csv')
 
-View(cocktails)
-cocktails$drink<-as.factor(cocktails$drink)
-numb.ingredients<-cocktails%>%
-  count(drink)
-View(numb.ingredients)
-
-View(boston_cocktails)
+boston_cocktails$category<-as.factor(boston_cocktails$category)
 boston_cocktails$ingredient<-as.factor(boston_cocktails$ingredient)
+
+#Heatmap of counts of ingredients and drink categories
+nlevels(boston_cocktails$ingredient)
+#There are 569 ingredients, going to take the top 10
+
 popular.ingredients<-boston_cocktails%>%
   group_by(ingredient)%>%
   tally()%>%
-  arrange(desc(n))
-View(popular.ingredients)
-nlevels(popular.ingredients$ingredient)
-#Oh wow there are 569 ingredients. 
-popular.ingredients$category<-as.factor(popular.ingredients$category)
-nlevels(popular.ingredients$category)
-#11 categories, so let's filter for the top 11 ingredients
+  arrange(desc(n))%>%
+  top_n(10)
+popular.ingredients
+#Top 10 ingredients
+#Gin, Fresh lemon juice, Simple Syrup, Vodka, Light Rum, Dry Vermouth, Fresh Lime Juice, 
+#Triple Sec, Powdered Sugar, Grenadine
 
-popular.ingredients<-boston_cocktails%>%
+data<-boston_cocktails%>%
   group_by(category, ingredient)%>%
   tally()%>%
-  desc(ingredient)
-top_n(11, ingredient)%>%
-  droplevels
+  filter(ingredient == "Gin"|ingredient == "Fresh lemon juice"|ingredient == "Simple Syrup"|ingredient == "Vodka"|ingredient == "Light Rum"|ingredient == "Dry Vermouth"|ingredient == "Fresh Lime Juice"|ingredient == "Triple Sec"|ingredient == "Powdered Sugar"|ingredient == "Grenadine")
 
+pal=pnw_palette("Shuksan2",100)
+plot<-ggplot(data, aes(category, ingredient, fill= n)) + 
+  geom_tile()+
+  theme_classic()+
+  scale_fill_gradientn(colours = pal) +
+  theme(plot.title = element_text(size=16, hjust=0.5, face="bold"), axis.text.x=element_text(angle = 45, hjust = 1, size=14), axis.text.y=element_text(size=14), axis.title.y = element_text(face="bold", size=16), axis.title.x = element_text(face="bold", size=16),
+        panel.grid.major=element_blank(), panel.grid.minor=element_blank()) +
+  ggtitle("Raw Counts")+
+  labs(x="Drink Category", y="Drink Ingredient", fill="Count")
+#I would like to get it scaled by total drinks from categories dataset so it's proportion instead of counts
 
-desc(ingredient)
+summary.data<-boston_cocktails%>%
+  group_by(category, ingredient)%>%
+  tally()
+View(summary.data)
+
+summary.categories<-summary.data%>%
+  group_by(category)%>%
+  summarize(total.drinks=sum(n))
+summary.categories
+
+#Combine data
+combined.full.data<-full_join(summary.data, summary.categories, by = "category", copy = TRUE)
+#Divide count by total drinks for each category to get proportion
+combined.full.data$percent<-(combined.full.data$n/combined.full.data$total.drinks)*100
+#Now filter to get dataset of just top 10 ingredients
+combined.full.data.top.ing<-combined.full.data%>%
+  filter(ingredient == "Gin"|ingredient == "Fresh lemon juice"|ingredient == "Simple Syrup"|ingredient == "Vodka"|ingredient == "Light Rum"|ingredient == "Dry Vermouth"|ingredient == "Fresh Lime Juice"|ingredient == "Triple Sec"|ingredient == "Powdered Sugar"|ingredient == "Grenadine")
+
+percent.plot<-ggplot(combined.full.data.top.ing, aes(category, ingredient, fill= percent)) + 
+  geom_tile()+
+  theme_classic()+
+  scale_fill_gradientn(colours = pal) +
+  theme(plot.title = element_text(size=16, hjust=0.5, face="bold"), axis.text.x=element_text(angle = 45, hjust = 1, size=14), axis.text.y=element_text(size=14), axis.title.y = element_text(face="bold", size=16), axis.title.x = element_text(face="bold", size=16),
+        panel.grid.major=element_blank(), panel.grid.minor=element_blank()) +
+  ggtitle("Proportion")+
+  labs(x="Drink Category", y="", fill="Percent")
+
+#Combine two plots into one 
+combined.plots<-ggarrange(plot, percent.plot,
+          ncol = 2, nrow = 1)
+#Add title to combined plot
+combined.plots.title<-annotate_figure(combined.plots,
+                top = text_grob("Use of top 10 ingredients across drink categories - counts vs proportion", color = "black", face = "bold", size = 20,hjust=0.5))
+combined.plots.title+ggsave("TT_cocktails_plot.png",width=10, height=5 )
+
 
