3237dd98e20a3a9c4d513ce0a5892cfef1c7aa15
Jennica Moffat <jennica.moffat.94@my.csun.edu>
2020-08-04 19:51:09 -0700

    How to order axes and legend

--- R_Things_I_Forget.R
+++ R_Things_I_Forget.R
@@ -1,24 +1,36 @@
 #Common things that I forget
 
+#Filtering data
+Data2<-Data%>%
+  filter(Column=="criteria")%>%
+  droplevels()
 
 #Tells you how many levels, or unique categories, are in that column. Must be a factor. 
 nlevels(data$column)
 
-
-summary(data$column)
 #Tells you the count of each level of that column
+summary(data$column)
 
 #Removing/keeping specific columns
-coffee.data2 = subset(coffee.data, select = c(aroma, flavor, aftertaste, acidity, body, balance, uniformity, clean_cup, sweetness, cupper_points))
 #Keeps columns specified
-df = subset(mydata, select = -c(x,z) )
+coffee.data2 = subset(coffee.data, select = c(aroma, flavor, aftertaste, acidity, body, balance, uniformity, clean_cup, sweetness, cupper_points))
 #Removes columns specified
+df = subset(mydata, select = -c(x,z) )
 
-coffee.long<-gather(data=coffee.data2, key='criteria', value='grade', aroma, flavor, aftertaste, acidity, body, balance, uniformity, clean_cup, sweetness, cupper_points)
 #Rotate data long
+coffee.long<-gather(data=coffee.data2, key='criteria', value='grade', aroma, flavor, aftertaste, acidity, body, balance, uniformity, clean_cup, sweetness, cupper_points)
 
 #Saving plot as png file
 plot+ggsave("Folder/Name.png", width=10, height=5)
 
 #Gather data long
 mydata2<-gather(data=mydata, key='Name of new column', value='value', column1, column2, etc.)
+
+#Ordering x-axis within ggplot
+data %>%
+  mutate(country = fct_reorder(country, rounded_avg, .fun='sum')) %>%
+  ggplot(aes(x=country, y=rounded_avg, fill=type))
+#order things manually in dataframe
+energy_avg2<-energy_avg%>%
+  mutate(type=fct_relevel(type, "Conventional thermal", "Geothermal", "Hydro", "Nuclear", "Solar", "Wind", "Other"))
+#More ways to reorder things: https://www.r-graph-gallery.com/267-reorder-a-variable-in-ggplot2.html
